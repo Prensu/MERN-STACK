04f9c60a9c3bfe0a3bf3e5a40b73b0c761defda2
Prensu <136965950+Prensu@users.noreply.github.com>
2025-04-22 19:01:08 +0545

    April 22nd

---
+++ React Native/p07 tabs.js
@@ -0,0 +1,22 @@
+//In react native , tab is a component used to create bottomtab navigation 
+import {StyleSheet, Text, View} from 'react-native'
+import React from 'react'
+import { Tabs } from 'expo-router';
+
+
+const _Layout = () => {
+    return (
+
+        <Tabs>
+            <Tabs.Screen
+            name="index"
+            options={{
+                title: 'Home',
+                headerShown: false
+            }}
+            />
+        </Tabs>
+    )
+}
+export default _Layout
+const styles = StyleSheet.create({})
